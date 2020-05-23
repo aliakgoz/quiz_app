69e12dde8e592991dc2c07a6dff880982f3ebb6f
aliakgoz <aliakgoz@gmail.com>
2020-05-23 15:53:37 +0300

    first app v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+// same with the line below
+void main() => runApp(MyApp());
+
 // extends uses another (prebuilt) class blueprint and if you want you can add properties to it. 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(home: Text('Hello!'),);
   }
